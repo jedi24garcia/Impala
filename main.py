0ac3a3fb5dda2c3430dca22125b16d9803ef3d4d
jedi24garcia <jedi24garcia@gmail.com>
2022-12-18 11:52:29 +1300

    created table

--- main.py
+++ main.py
@@ -2,6 +2,24 @@
 
 from random import randint
 
-board = []
-
 print("Let's play fucking Battleship!")
+
+board = [
+    [' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' '],
+]
+
+letters_to_numbers = {
+  'A': 0,
+  'B': 1,
+  'C': 2,
+  'D': 3,
+  'E': 4,
+}
+
+for row in board:
+  print(row)
+
