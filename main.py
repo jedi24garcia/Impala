a35273dc8027d188078f15624f8a92843541b182
jedi24garcia <jedi24garcia@gmail.com>
2022-12-19 10:03:31 +1300

    learning

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from random import randint
 
 print("Let's play fucking Battleship!")
 
+Hidden_Pattern = [[' ']*8 for x in range(6)]
+Guess_Pattern = [[' ']*8 for x in range(6)]
+
+"""
 board = [
     [' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' '],
@@ -11,24 +15,23 @@ board = [
     [' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' '],
 ]
+"""
 
-letters_to_numbers = {
-  'A': 0,
-  'B': 1,
-  'C': 2,
-  'D': 3,
-  'E': 4,
-}
+let_to_num = {'A':0,'B':1, 'C':2,'D':3,'E':4,'F':5}
 
-for n in range(5):
+def print_board(board):
+  print('A B C D E F')
+  print('*****')
+  row_num = 1
+  for row in board:
+    print("%d|%s|" % (row_num, "I".join(row)))
+    row_num +=1
+
+for n in range(6):
   print("Where you want ship ", n + 1, "?")
-  column = input("column (A to E):")
-  row = input("row (1 to 5):")
-  column_number = letters_to_numbers[column]
+  column = input("column (A to F):")
+  row = input("row (1 to 6):")
+  column_number = let_to_num[column]
   row_number = int(row) - 1
 
-board[row_number][column_number] = 'X'
-
-for row in board:
-  print(row)
-
+# TODO: need to print board
