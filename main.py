5cbf772bca11f299efc6ab2c790e7b5e1ac52b1d
jedi24garcia <jedi24garcia@gmail.com>
2022-12-23 12:45:35 +1300

    recreating new

--- main.py
+++ main.py
@@ -1,67 +1,10 @@
 #!/usr/bin/env python3
 
 from random import randint
+import os
 
 print("Let's play fucking Battleship!")
 
-Hidden_Pattern = [[' '] *8 for x in range(6)]
-Guess_Pattern = [[' '] *8 for i in range(6)]
-
-"""
-board = [
-    [' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' '],
-]
-"""
-
-class Battleboard:
-  def __init__(self, board, width, height):
-    self.board = board
-    self.width = width
-    self.height = height
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class battleboard:
+  def __init__(self, size):
+    self.size = size
