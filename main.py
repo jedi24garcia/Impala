d43c8cce274dec4928c31ffa45aa04fddd09bf44
jedi24garcia <jedi24garcia@gmail.com>
2023-01-10 13:43:44 +1300

    trial

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import sys 
 
-a = np.array([[1,2,3],
-              [4,5,6]])
-
-b = np.array([[10,11,12],
-              [13,14,15]])
-
-c = a + b
-
-print(c)
+for i in range(10):
+  print(i)
