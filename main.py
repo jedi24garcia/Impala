5f07d76fa3675c1ba5f62aa6d8073809d95edb97
jedi24garcia <jedi24garcia@gmail.com>
2023-10-06 12:05:19 +1300

    starting

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print("hello,world")
+import speech_recognition as sr
+import pyttsx3
+
+engine = pyttsx3.init()
+
+def speak():
+  engine.say(text)
+  engine.runAndWait()
+  engine.stop()
+
+
+speak("hello")
