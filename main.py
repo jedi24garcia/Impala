e105089709607b170e9b748c84fd991f339e4572
jedi24garcia <jedi24garcia@gmail.com>
2023-10-06 14:06:18 +1300

    seems ok

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 
 import speech_recognition as sr
-import pyttsx3
+from AppKit import NSSpeechSynthesizer
+from Foundation import NSSpeechSynthesizer, NSArray
 
-engine = pyttsx3.init()
+def getVoice():
+  voices = NSSpeechSynthesizer.availableVoices()
+  voicenames = []
+ 
+  for voice in voices:
+    voiceattr = NSSpeechSynthesizer.attributesForVoice_(voice)
+    voicename = voiceattr['VoiceName']
+    if voicename not in voicenames:
+      voicenames.append(voicename)
+  return voicenames
 
-def speak():
-  engine.say(text)
-  engine.runAndWait()
-  engine.stop()
+def speak(text, voicename):
+  synthesizer = NSSpeechSynthesizer.alloc().initWithVoice_(voicename)
+  if synthesizer:
+    synthesizer.startSpeakingString_(text)
+  else:
+    print(f"Voice '{voicename}' is not available.")
 
+if __name__ == '__main__':
+  voicenames = getVoice()
+  if not voicenames:
+    print("Unavailable")
+  else:
+    for voicename in voicenames:
+      print(voicename) 
 
-speak("hello")
+      selected_persona = voicenames[0]
+      text_to_speak = "Hello"
+
+      speak(text_to_speak, selected_persona)
