77819cb0b095f1f9ed6e1222d77dfa7469f8bb6d
jedi24garcia <jedi24garcia@gmail.com>
2023-10-24 12:15:44 +1300

    changed

---
+++ practical/validate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# import os
+
+# print("Welcome to the expression validating program.")
+
+"""
+expression = "abc"
+x = "Y"
+y = "N"
+
+while True:
+  user = input("Enter your expression: ")
+  if user == expression:
+    break
+  else:
+    if user is not expression:
+      print("try again")
+"""
+
+def prime():
+  print("Welcome to the expression validating program.")
+
+def ExpValidator(expression, open_char, close_char):
+  print("hello")
+
+if __name__ == "__prime__":
+  prime()
