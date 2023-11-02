1dd91a6fbd89af0c59bc2f7c11a5ee09d1edc2df
jedi24garcia <jedi24garcia@gmail.com>
2023-11-03 12:42:45 +1300

    mods comments

--- practical/validate.py
+++ practical/validate.py
@@ -51,7 +51,12 @@ if __name__ == "__main__":
   main()
 
 # 1. Why did you select that specific data structure?
-# This data structure allows users to enter an expression and would validate that expression if its valid or not.
+# This structure allows users to enter an expression and would validate that expression if its valid or not. 
+# A stack is the data structure used in this program. I chose this structure as it allows for an
+# efficient checking of matching brackets in an expression. 
 # 2. How was that data structure suited to the task?
-# The data structure suited the task as  
+# The stack data structure suited the task as the structure is well-suited to efficiently track
+# and authenticate bracket pairs in expressions. This is what requires in the task for validating expressions with matching brackets.
 # 3. Could another data structure be used to complete the same task? If so, how would your solution differ?
+# Yes, you can use queue data structure however it may result to less efficient. I believe stack
+# is more straightforward and more efficient.
