014247ce35b5c51c8cb8159c5cbee0d8653e40b7
jedi24garcia <jedi24garcia@gmail.com>
2023-11-03 13:30:10 +1300

    mods comment val

--- practical/validate.py
+++ practical/validate.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 
-class UserValidator:
-  def __init__(self, opening_bracket, closing_bracket):
-    self.stack = []
+class UserValidator: # validates expressions
+  def __init__(self, opening_bracket, closing_bracket): # initializes objects with open and close brackets
+    self.stack = [] # keeps track of open brackets
     self.opening_bracket = opening_bracket
     self.closing_bracket = closing_bracket
 
-  def valid_expression(self, UserExpression):
+  def valid_expression(self, UserExpression): # validates users inputs
     self.stack = [] # creating the stack
-    self.UserExpression = UserExpression      
+    self.UserExpression = UserExpression
+    # loop repeats for each character with self.UserExpression     
     for char in self.UserExpression:
-      if char == self.opening_bracket:
+      if char == self.opening_bracket: # pushed onto the stack
         self.stack.append(char)
-      elif char == self.closing_bracket:
+      elif char == self.closing_bracket: # checks if stack is empty
         if not self.stack:
-          return False
+          return False # returns False if closing bracket is not matched
         if self.stack[-1] == self.opening_bracket:
           self.stack.pop()
         else:
           return False
       
-    return len(self.stack) == 0
+    return len(self.stack) == 0 # the code checks if opening bracket is not matched after characters
+    # has been processed. If there are, this will return to False. If not, it returns True
 
-def main():
+# main program
+def main(): # entry point
   print("Welcome to the expression validating program.")
   
-  while True:
+  while True: # loops 
     try:  
     # expression from the user
       UserExpression = input(f"Enter your expression: ")
@@ -35,6 +38,8 @@ def main():
     opening_bracket = input(f"Enter the opening brackets: ")
     closing_bracket = input(f"Enter the closing brackets: ")
 
+    # creates instance of UserValidator class that would pass the opening and closing 
+    # bracket characters given by the user
     validator = UserValidator(opening_bracket, closing_bracket)
     
     if validator.valid_expression(UserExpression):
