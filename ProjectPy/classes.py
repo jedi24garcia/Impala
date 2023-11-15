008da99896f6e326a222d81e3b8b81b00e612655
jedi24garcia <jedi24garcia@gmail.com>
2023-11-16 09:17:38 +1300

    added new file for classes

---
+++ ProjectPy/classes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+print("**** Welcome TO WHITECLIFFE College of Information Technology ****")
+print("******************** STUDENT PORTAL *************************")
