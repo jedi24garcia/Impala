1bd40ec09d114a181847228986e8e8648353b1ea
jedi24garcia <jedi24garcia@gmail.com>
2023-11-26 18:42:38 +1300

    removed faker generate def

--- ProjectPy/classes.py
+++ ProjectPy/classes.py
@@ -4,13 +4,8 @@
 
 
 import random
-from faker import Faker
-# import mimesis
-
 
 class Student:
-  faker = Faker()
-
   def __init__(self, firstname="", lastname="", email="", campus=""): 
     self.firstname = firstname
     self.lastname = lastname
@@ -23,27 +18,6 @@ class Student:
     self.lastname = input("Last Name: ")
     self.email = input("Email Address: ")
     self.campus = input("Campus: ")
-
-  def generate_info(self):
-    self.firstname_generate()
-    self.lastname_generate()
-    self.email_generate()
-    self.campus_generate()
-
-  def firstname_generate(self):
-    # faker = Faker()
-    self.firstname = self.faker.first_name()
-
-  def lastname_generate(self):
-    # faker = Faker
-    self.lastname = self.faker.last_name()
-
-  def email_generate(self):
-    self.email = f'{self.firstname.lower()}.{self.lastname.lower()}@whitecliffe.ac.nz'
-
-  def campus_generate(self):
-    campuses = ["Auckland, Wellington, Christchurch"]
-    self.campus = random.choice(campuses)
 
   def __str__(self):
     return "{} {} {} {}".format(
